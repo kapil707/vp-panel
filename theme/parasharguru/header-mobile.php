f197b78151414f2cea1fad66e0808a8cd57ab99d
kapil707 <kapil707sharma@gmail.com>
2023-10-04 16:17:47 +0530

    add

--- theme/parasharguru/header-mobile.php
+++ theme/parasharguru/header-mobile.php
@@ -9,22 +9,19 @@
 <![endif]-->
 
 <head>
-    <!-- TITLE OF SITE -->
-    <title><?php echo bloginfo('name'); ?><?php echo wp_title(); ?> | <?php echo bloginfo('description'); ?></title>
+	<?php echo vp_head(); ?>
 
-      <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
-  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.slim.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
-      <link href="<?php echo get_template_directory_uri();?>/css/login_style.css" rel="stylesheet">
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
-
-	<?php wp_head(); ?>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+	<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.slim.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+	<link href="<?php echo get_template_directory_uri();?>/css/login_style.css" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
 </head>
 <body>
 <?php
@@ -34,11 +31,11 @@ $id = $_SESSION['profile_user'];
 if($id==""){
     wp_redirect(home_url()."/login");
 }
-
+/*
 global $wpdb;
 $table_name = $wpdb->prefix . 'my_users';
 $sql = "SELECT * FROM $table_name WHERE id='$id'";
-$users_row = $wpdb->get_row($sql);
+$users_row = $wpdb->get_row($sql); */
 ?>
 <div class="mobile-left-menu">
 	<i class="fa fa-times mobile-left-menu-cancel" aria-hidden="true" onclick="menuoff()"></i>
