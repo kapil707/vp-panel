3289f8c84361fc98046c7a26039f1cd39c302381
kapil707 <kapil707sharma@gmail.com>
2023-10-04 16:22:13 +0530

    add

--- theme/parasharguru/header-mobile.php
+++ theme/parasharguru/header-mobile.php
@@ -17,7 +17,7 @@
 	<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.slim.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
-	<link href="<?php echo get_theme_path(); ?>/css/login_style.css" rel="stylesheet">
+	<link href="<?php echo get_theme_path(); ?>css/login_style.css" rel="stylesheet">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -41,7 +41,7 @@ $users_row = $wpdb->get_row($sql); */
 	<i class="fa fa-times mobile-left-menu-cancel" aria-hidden="true" onclick="menuoff()"></i>
 	<div class="row">
 		<div class="col-sm-12 text-center">
-			<img src="<?php echo get_template_directory_uri();?>/images/user-account.png" width="50%" class="rounded-circle img-thumbnail user_image">
+			<img src="<?php echo get_theme_path(); ?>images/user-account.png" width="50%" class="rounded-circle img-thumbnail user_image">
 		</div>
 		<div class="col-sm-12 text-center text-white">
 			<?php echo $users_row->first_name ?>
@@ -93,7 +93,7 @@ $users_row = $wpdb->get_row($sql); */
 <div class="fix-header bg-[#A17603]">
 	<div class="col-sm-12">
 		<div class="bg-[#A17603] text-white text-center p-1">
-			<img src="<?php echo get_template_directory_uri();?>/images/menu-icon-12.png" class="mobile-menu-css" onclick="menushow()">
+			<img src="<?php echo get_theme_path(); ?>images/menu-icon-12.png" class="mobile-menu-css" onclick="menushow()">
 			<p class="font-medium text-[28px] mt-1" id="join" style="font-size: 35px;">
 				<?php echo get_field('login_label1','43'); ?>
 			</p>
