cf583cbbbe48f737494d20870307013844b1b2ce
kapil <kapil707sharma@gmail.com>
2023-10-07 13:10:42 +0530

    add

---
+++ application/views/admin/manage_whatsapp/add.php
@@ -0,0 +1,49 @@
+<div class="row">
+	<div class="col-xs-12" style="margin-bottom:20px;">
+		<a href="<?= base_url()?>admin/<?= $Page_name; ?>" class="btn btn-w-m btn-info">
+			Back
+		</a>
+	</div>
+	<form class="form-horizontal" role="form" method="post" enctype="multipart/form-data">
+		<div class="col-xs-9">
+			<div class="ibox float-e-margins">
+				<div class="ibox-content">
+					<div class="form-group">
+						<div class="col-sm-12">
+							<label class="control-label" for="form-field-1">
+								Number (Eg. 919530005050 no need +)
+							</label>
+						</div>
+						<div class="col-sm-12">
+							<input type="text" class="form-control" id="form-field-1" placeholder="Title" name="title" value="" />
+						</div>
+						<div class="help-inline col-sm-12 has-error">
+							<span class="help-block reset middle">  
+								<?= form_error('title'); ?>
+							</span>
+						</div>
+					</div>
+					
+					<div class="form-group">
+						<div class="col-sm-12">
+							<label class="control-label" for="form-field-1">
+								Message
+							</label>
+						</div>
+						<div class="col-sm-12">
+							<textarea type="text" class="form-control" id="form-field-1" placeholder="Message" name="message" style="height:100px"><?php echo set_value('message'); ?></textarea>
+						</div>
+						<div class="help-inline col-sm-12 has-error">
+							<span class="help-block reset middle">  
+								<?= form_error('message'); ?>
+							</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-xs-3">
+			<?php publish_panel_right_top("","Submit"); ?>
+		</div>
+	</form>
+</div>
