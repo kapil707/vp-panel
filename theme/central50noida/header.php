a9bf1e0a270825628643292e2176ef4195d72f46
kapil707 <kapil707sharma@gmail.com>
2023-10-08 15:57:33 +0530

    add

--- theme/central50noida/header.php
+++ theme/central50noida/header.php
@@ -16,6 +16,8 @@
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
+    <link rel="stylesheet" href="<?php echo get_theme_path(); ?>css-1/style3661.css?v=2.0">
+
     <?php /*
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
