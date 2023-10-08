fa82d2e7606d2437f646d82027018781bbae4fe5
kapil707 <kapil707sharma@gmail.com>
2023-10-08 15:55:26 +0530

    add

--- theme/central50noida/footer.php
+++ theme/central50noida/footer.php
@@ -118,3 +118,19 @@ $('.get-in-touch-form-btn, .close-form').click(function(){
 </style>
 </body>
 </html>
+<script>
+document.addEventListener("DOMContentLoaded", function(){
+  window.addEventListener('scroll', function() {
+      if (window.scrollY > 50) {
+        document.getElementById('header').classList.add('fixed-top');
+        // add padding top to show content behind navbar
+        navbar_height = document.querySelector('.navbar').offsetHeight;
+        document.body.style.paddingTop = navbar_height + 'px';
+      } else {
+        document.getElementById('header').classList.remove('fixed-top');
+         // remove padding top from body
+        document.body.style.paddingTop = '0';
+      } 
+  });
+}); 
+</script>
