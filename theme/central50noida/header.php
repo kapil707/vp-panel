545a97265f1eff37278f27c3a75ddeb35d71b17d
kapil707 <kapil707sharma@gmail.com>
2023-10-08 14:10:10 +0530

    add

--- theme/central50noida/header.php
+++ theme/central50noida/header.php
@@ -2,6 +2,11 @@
 <html lang="en-US" class="no-js">
 <head>
     <?php echo vp_head(); ?>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    
     <link rel="stylesheet" href="<?php echo get_theme_path(); ?>css/plugins.min.css">
     <link rel="stylesheet" href="<?php echo get_theme_path(); ?>css/revslider/settings.css">
     <link rel="stylesheet" href="<?php echo get_theme_path(); ?>css/style3661.css?v=2.0">
--- theme/central50noida/index.php
+++ theme/central50noida/index.php
@@ -1,7 +1,4 @@
 <?php include_once(get_header()); ?>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 <section id="home" class="fullwidth bg-stone-800">
     <?php echo do_slider(0) ;?>
 </section>
