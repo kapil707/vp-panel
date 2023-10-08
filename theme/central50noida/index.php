8c30af06a950e97d581c58bdf93dedc93dc6008d
kapil707 <kapil707sharma@gmail.com>
2023-10-08 14:53:38 +0530

    add

--- theme/central50noida/index.php
+++ theme/central50noida/index.php
@@ -69,7 +69,10 @@
                     <?php
                      $i = 1;						
                      foreach($get_blog as $row) { ?>
-                        <div class="tab-pane fade <?php if($i==1) { ?>show active<?php } ?>" id="home_<?php echo $row->id; ?>" role="tabpanel" aria-labelledby="home-tab_<?php echo $row->id; ?>"></div>
+                        <div class="tab-pane fade <?php if($i==1) { ?>show active<?php } ?>" id="home_<?php echo $row->id; ?>" role="tabpanel" aria-labelledby="home-tab_<?php echo $row->id; ?>">
+                            <h5><?php echo $row->title; ?></h5>
+                            <?php echo $row->description; ?>
+                        </div>
                     <?php 
                     $i++;
                     } ?>
