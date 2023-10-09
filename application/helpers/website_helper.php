b1384cbf1f8f1ea1b9bc75c12acccb2880d9eeaa
kapil707 <kapil707sharma@gmail.com>
2023-10-09 12:03:30 +0530

    add

--- application/helpers/website_helper.php
+++ application/helpers/website_helper.php
@@ -111,7 +111,7 @@ if ( ! function_exists('vp_menu'))
 				$dropdown = "dropdown";
 			}
 			
-			$return.= '<li class="nav-item'.$dropdown.'"><a href="'.base_url().$url.'" class="nav-link">'.$menu->title.$submenu.'</a>'.$submenu.'</li>';
+			$return.= '<li class="nav-item'.$dropdown.'"><a href="'.base_url().$url.'" class="nav-link">'.$menu->title.$submenu.'</a>'.$submenu.$menu->id.'</li>';
 		}
 		$return = '<nav class="navbar navbar-expand-lg"><button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button><div class="collapse navbar-collapse" id="navbarSupportedContent"><ul class="navbar-nav mr-auto">'.$return.'</li>
 		</ul></div></nav>';
