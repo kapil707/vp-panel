fb1f66a85da85d2e0e6767346f7a569349b1c410
kapil707 <kapil707sharma@gmail.com>
2023-10-09 14:15:19 +0530

    add

--- theme/central50noida/header.php
+++ theme/central50noida/header.php
@@ -64,7 +64,7 @@
                         <img src="<?php echo $logo = get_library_to_image(get_field_data("image_site_logo"),'main'); ?>" alt="logo" class="logo"></a>
                         </h2>
                     </div>
-                    <div class="col-md-8 col-8 float-right">
+                    <div class="col-md-8 col-8 web_menu">
 						<?php echo vp_menu(); ?>
                     </div>
                 </div>
