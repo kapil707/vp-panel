8b67e7f9a7d1eab1a81f8f4c5c82440cbf92dc14
kapil707 <kapil707sharma@gmail.com>
2023-10-09 12:27:15 +0530

    add

--- application/helpers/website_helper.php
+++ application/helpers/website_helper.php
@@ -104,14 +104,14 @@ if ( ! function_exists('vp_menu'))
 				$url = $dt["url"];
 			}
 
-			$dropdown = "";
 			$submenu = vp_menu_submenu($menu->id);
 			if(!empty($submenu))
 			{
-				$dropdown = "dropdown";
+				$return.= '<li class="nav-item dropdown"><a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">'.$menu->title.'</a>'.$submenu.'</li>';
 			}
-			
-			$return.= '<li class="nav-item'.$dropdown.'"><a href="'.base_url().$url.'" class="nav-link">'.$menu->title.'</a>'.$submenu.'</li>';
+			else{
+				$return.= '<li class="nav-item"><a href="'.base_url().$url.'" class="nav-link">'.$menu->title.'</a></li>';
+			}
 		}
 		$return = '<nav class="navbar navbar-expand-lg"><button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button><div class="collapse navbar-collapse" id="navbarSupportedContent"><ul class="navbar-nav mr-auto">'.$return.'</li>
 		</ul></div></nav>';
