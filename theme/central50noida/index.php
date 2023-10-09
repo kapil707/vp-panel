fbf415b1216f74ab4eff53bcc32c4c4ae621731d
kapil707 <kapil707sharma@gmail.com>
2023-10-09 13:32:22 +0530

    add

--- theme/central50noida/index.php
+++ theme/central50noida/index.php
@@ -190,7 +190,7 @@
     </div>
 </div>
 
-<div class="home-box6" class="home-box-main">
+<div id="home-box6" class="home-box-main">
     <div class="h-svg">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-collection-play" viewBox="0 0 16 16">
         <path d="M2 3a.5.5 0 0 0 .5.5h11a.5.5 0 0 0 0-1h-11A.5.5 0 0 0 2 3zm2-2a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 0-1h-7A.5.5 0 0 0 4 1zm2.765 5.576A.5.5 0 0 0 6 7v5a.5.5 0 0 0 .765.424l4-2.5a.5.5 0 0 0 0-.848l-4-2.5z"/>
