7e0be62dc17359a8fb26f5d60fff954072a468c1
kapil707 <kapil707sharma@gmail.com>
2023-10-09 15:38:38 +0530

    add

--- theme/central50noida/about_us-single.php
+++ theme/central50noida/about_us-single.php
@@ -1,15 +1,16 @@
 <?php include_once(get_header()); ?>
-<section id="home" class="fullwidth bg-stone-800">
-    <div id="home-slider-wrapper" class="rev_slider_wrapper fullwidthbanner-container">
-        <img src="<?php echo get_library_to_image($page_data->image,'main'); ?>" width="100%">
-    </div>
-</section>
-<section id="about" class="lg-py-4 sm-py-4">
-    <div class="container cursor-default">
-        <div class="flex-row lg-flex-columns-12 md-flex-columns-2 sm-flex-columns-2 xs-flex-columns-1">
-            <h2 class="text-center"><?php echo $page_data->title ?></h2>
-            <?php echo $page_data->description ?>
+<div id="about-box1">
+    <img src="<?php echo get_library_to_image($page_data->image,'main'); ?>" width="100%">
+</div>
+
+<div id="about-box2">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-3 text-center">
+                <h5><?php echo $page_data->title ?></h5>
+                <?php echo $page_data->description ?>
+            </div>
         </div>
     </div>
-</section>
+</div>
 <?php include_once(get_footer()); ?>
