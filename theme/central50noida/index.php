4a1d1f0a6dd0cf3b5039183cb5ae527408a674ad
kapil707 <kapil707sharma@gmail.com>
2023-10-09 16:18:05 +0530

    add

--- theme/central50noida/index.php
+++ theme/central50noida/index.php
@@ -114,7 +114,7 @@
             foreach($get_blog as $row) { ?>
                 <div class="col-sm-4 col-6 text-center sml_border0">
                     <div>
-                        <span class="sml_border wow heartBeat">
+                        <span class="sml_border wow flip">
                             <?php echo $row->title; ?>
                         </span>
                     </div>
