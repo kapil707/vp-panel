ffb9406a8c584a736182cb6af315fb518ace4c84
kapil707 <kapil707sharma@gmail.com>
2023-10-10 14:16:05 +0530

    add

--- theme/parasharguru/edit-profile.php
+++ theme/parasharguru/edit-profile.php
@@ -99,3 +99,17 @@ function onchnage_dropdown(){
 }
 </script>
 <?php include_once(get_footer("mobile")); ?>
+
+<div class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+  <div class="toast-header">
+    <img src="..." class="rounded mr-2" alt="...">
+    <strong class="mr-auto">Bootstrap</strong>
+    <small class="text-muted">11 mins ago</small>
+    <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>
+  <div class="toast-body">
+    Hello, world! This is a toast message.
+  </div>
+</div>
