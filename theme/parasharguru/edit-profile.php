7f4ec71b1fc53716d0a98205e2054bda073ea989
kapil707 <kapil707sharma@gmail.com>
2023-10-10 14:19:34 +0530

    add

--- theme/parasharguru/edit-profile.php
+++ theme/parasharguru/edit-profile.php
@@ -113,6 +113,10 @@ function onchnage_dropdown(){
     Hello, world! This is a toast message.
   </div>
 </div>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
 <script>
 $(document).ready(function(){
   $('.toast').toast('show');
