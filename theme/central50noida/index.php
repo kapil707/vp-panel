8df9b7ff98ecec5a9c9a3b6ba173845c7da97d6a
kapil707 <kapil707sharma@gmail.com>
2023-10-11 11:13:54 +0530

    add

--- theme/central50noida/index.php
+++ theme/central50noida/index.php
@@ -270,28 +270,25 @@
 <script>
 $(document).ready(function(){
 
-$(".filter-button").click(function(){
-    var value = $(this).attr('data-filter');
-    
-    if(value == "all")
-    {
-        //$('.filter').removeClass('hidden');
-        $('.filter').show('1000');
-    }
-    else
-    {
-//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
-//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
-        $(".filter").not('.'+value).hide('3000');
-        $('.filter').filter('.'+value).show('3000');
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
         
-    }
-});
+        if(value == "all")
+        {
+            $('.filter').show('1000');
+        }
+        else
+        {
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+            
+        }
+    });
 
-if ($(".filter-button").removeClass("active")) {
-$(this).removeClass("active");
-}
-$(this).addClass("active");
+    if ($(".filter-button").removeClass("active")) {
+        $(this).removeClass("active");
+    }
+    $(this).addClass("active");
 
 });
 </script>
