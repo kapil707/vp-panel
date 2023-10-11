0589ab1d519175fb9a555190da9807e887841459
kapil707 <kapil707sharma@gmail.com>
2023-10-11 13:13:58 +0530

    add

--- theme/central50noida/construction-updates.php
+++ theme/central50noida/construction-updates.php
@@ -1,9 +1,32 @@
 <?php include_once(get_header()); ?>
-<div id="about-box1">
-    <img src="<?php echo get_library_to_image($page_data->image,'main'); ?>" class="main-banner mobile_off">
-    <img src="<?php echo get_library_to_image($page_data->mobile_image,'main'); ?>" class="main-banner mobile_show">
+<div id="slider">
+    <?php echo do_slider("slider") ;?>
 </div>
+<style>
+#myCarousel_slider {
+    width: 100%;
+    max-width: 100%;
+}
+.carousel-inner {
+    width: 100%;
+    max-width: 100%;
+    height: 650px; /* Adjust the height as needed */
+}
 
+.carousel-item img {
+    width: 100%;
+    max-width: 100%;
+    height: 650px;
+}
+@media screen and (max-width:800px) {
+    .carousel-inner {
+        height: 250px !important;
+    }
+    .carousel-item img {
+        height: 250px !important;
+    }
+}
+</style>
 <div id="about-box2">
     <div class="container-fluid">
         <div class="row">
