5e1fc4af4320100d348a4796708f3a7f556eb52b
kapil707 <kapil707sharma@gmail.com>
2023-10-11 12:49:38 +0530

    add

--- application/helpers/admin_helper.php
+++ application/helpers/admin_helper.php
@@ -214,11 +214,11 @@ if ( ! function_exists('get_page_template'))
 						?>
 						<select class="form-control">
 							<option>Select Template</option>
-							<?php foreach($map as $r){
-								if(strpos($r,'.php')){
-								?>
-									<option <?php if(!empty($row->link_page)) { if($row->link_page.".php"==$r) { echo "selected"; } }?>>
-										<?php echo $r; ?>
+							<?php foreach($map as $row){
+								if(strpos($row,'.php')){
+									$val = str_replace(".php","",$row); ?>
+									<option <?php if(!empty($row->link_page)) { if($row->link_page.".php"==$row) { echo "selected"; } }?> value="<?php echo $val; ?>">
+										<?php echo $row; ?>
 									</option>
 								<?php
 								}
