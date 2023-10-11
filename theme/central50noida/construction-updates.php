92afd91614bea821c64f1d68781d35cb483e903b
kapil707 <kapil707sharma@gmail.com>
2023-10-11 13:15:33 +0530

    add

--- theme/central50noida/construction-updates.php
+++ theme/central50noida/construction-updates.php
@@ -36,31 +36,26 @@
             <div class="col-md-12">
                 <?php echo $page_data->description ?>
             </div>
+            <?php 
+            $get_blog = get_blog("construction_updates","limit 0,4");
+            $i = 1;						
+            foreach($get_blog as $row) { ?>
+                <div class="col-sm-3">
+                    <div class="item filter construction_update">
+                        <a class="mobile_off" href="<?php echo base_url(); ?>construction-updates/<?php echo $row->id; ?>">
+                            <?php $img = get_library_to_image($row->image,'main'); ?>
+                            <img src="<?= $img; ?>" class="img-fluid mobile_off">
+                        </a>
+                        <a class="mobile_show" href="<?php echo base_url(); ?>construction-updates/<?php echo $row->id; ?>">
+                            <?php $img = get_library_to_image($row->mobile_image,'main'); ?>
+                            <img src="<?= $img; ?>" class="img-fluid mobile_show">
+                        </a>
+                        <h4><?php echo $row->title; ?></h4>
+                    </div>
+                </div>
+            <?php 
+            } ?>
         </div>
     </div>
 </div>
-<section class="contact-sec lg-py-4 sm-py-4">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8">
-                <?php
-				$get_blog = get_blog();
-				foreach($get_blog as $row){?>
-				<div class="row" style="margin-bottom:20px;">
-					<div class="col-sm-5">
-						<img src="<?php echo get_library_to_image($row->image,'main'); ?>" width="100%">
-					</div>
-					<div class="col-sm-7">
-						<?php echo $row->title; ?>
-					</div>
-				</div>
-				<?php } ?>
-            </div>
-			
-            <div class="col-md-6 mob-m-t-50">
-                
-            </div>
-        </div>
-    </div>
-</section>
 <?php include_once(get_footer()); ?>
