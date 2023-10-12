56a8aeb4999037b7273b7438acbb12ba351eeab9
kapil707 <kapil707sharma@gmail.com>
2023-10-12 15:25:08 +0530

    add

--- theme/parasharguru/change-password.php
+++ theme/parasharguru/change-password.php
@@ -26,8 +26,8 @@ $result1 = get_table("tbl_o_interest");
 			<label>Re-enter Password:</label>
 			<input type="password" name="password" class="form-control input-lg" placeholder="Re-enter Password" required="" value="" onchange="check_password2()" id="renew_password">
 		</div>
-		<div class="col-sm-12 form-group">
-			<div class="alert alert-success"></div>
+		<div class="col-sm-12 form-group alert_div">
+			
 		</div>
 	</div>
 </form>	
@@ -38,7 +38,7 @@ function check_password1()
 	if(new_password.length < 8)
 	{
 		swal("Password must contain 8 characters (e.g. A,a or 1,2 or !,$,@)");
-		$(".alert_div").html("<p class='text-danger'>Password must contain 8 characters (e.g. A,a or 1,2 or !,$,@)</p>");
+		$(".alert_div").html("<div class='alert alert-danger'>Password must contain 8 characters (e.g. A,a or 1,2 or !,$,@)</div>");
 		// pass2 = 0;
 		// submit_btn();
 	}
@@ -57,13 +57,13 @@ function check_password2()
 	if(new_password!=renew_password)
 	{
 		swal("Password doesn't match");
-		$(".alert_div").html("<p class='text-danger'>Password doesn't match</p>");
+		$(".alert_div").html("<div class='alert alert-danger'>Password doesn't match</div>");
 		// pass3 = 0;
 		// submit_btn();
 	}
 	else
 	{
-		$(".alert_div").html("<p class='text-success'>Password Matched.</p>");
+		$(".alert_div").html("");
 		// pass3 = 1;
 		// submit_btn();
 	}
