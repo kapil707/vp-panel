f5c751714aab84de4a731d5131df8cffa394896d
kapil707 <kapil707sharma@gmail.com>
2023-10-12 15:19:14 +0530

    add

--- theme/parasharguru/change-password.php
+++ theme/parasharguru/change-password.php
@@ -26,45 +26,46 @@ $result1 = get_table("tbl_o_interest");
 			<label>Re-enter Password:</label>
 			<input type="password" name="password" class="form-control input-lg" placeholder="Re-enter Password" required="" value="" onchange="check_password2()" id="renew_password">
 		</div>
+		<div class="col-sm-12 form-group">
+			<div class="alert alert-success"></div>
+		</div>
 	</div>
 </form>	
 <script>
 function check_password1()
 {
-	$(".check_new_password_div").html("Loading....");
 	new_password = $("#new_password").val();
 	if(new_password.length < 8)
 	{
 		swal("Password must contain 8 characters (e.g. A,a or 1,2 or !,$,@)");
-		$(".submit_div").html("<p class='text-danger'>Password must contain 8 characters (e.g. A,a or 1,2 or !,$,@)</p>");
-		pass2 = 0;
-		submit_btn();
+		$(".alert_div").html("<p class='text-danger'>Password must contain 8 characters (e.g. A,a or 1,2 or !,$,@)</p>");
+		// pass2 = 0;
+		// submit_btn();
 	}
 	else
 	{
-		$(".submit_div").html("&nbsp;");
-		pass2 = 1;
-		submit_btn();
+		$(".alert_div").html("&nbsp;");
+		// pass2 = 1;
+		// submit_btn();
 	}
 }
 function check_password2()
 {
-	$(".check_renew_password_div").html("Loading....");
 	new_password = $("#new_password").val();
 	renew_password = $("#renew_password").val();
 	check_password1();
 	if(new_password!=renew_password)
 	{
 		swal("Password doesn't match");
-		$(".submit_div").html("<p class='text-danger'>Password doesn't match</p>");
-		pass3 = 0;
-		submit_btn();
+		$(".alert_div").html("<p class='text-danger'>Password doesn't match</p>");
+		// pass3 = 0;
+		// submit_btn();
 	}
 	else
 	{
-		$(".submit_div").html("<p class='text-success'>Password Matched.</p>");
-		pass3 = 1;
-		submit_btn();
+		$(".alert_div").html("<p class='text-success'>Password Matched.</p>");
+		// pass3 = 1;
+		// submit_btn();
 	}
 }
 </script>
