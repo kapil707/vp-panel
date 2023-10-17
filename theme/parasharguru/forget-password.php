34e874b00344b2cb1f2189174e7ac9616d6ff336
kapil <kapil707sharma@gmail.com>
2023-10-17 16:51:41 +0530

    add

---
+++ theme/parasharguru/forget-password.php
@@ -0,0 +1,17 @@
+<?php include_once(get_header("mobile")); ?>
+<?php
+include_once('functions.php');
+$id  = $_SESSION["temp_user_id"];
+$dt = array(
+	'password' =>'',
+);
+$where = array('id'=>$id);
+edit_function("tbl_o_my_users",$dt,$where);
+
+/*************************************************** */
+$row = get_table_row("tbl_o_my_users where id='$id'");
+$otp = getName();
+$message = "Hello $row->name <br>Your otp to login https://www.parashar.guru/ is : $otp";
+send_otp($message,$row->mobile);
+?>
+<?php include_once(get_footer("mobile")); ?>
