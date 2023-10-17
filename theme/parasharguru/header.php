5b6f729494a9570040e586e6b235b4b16574b8e6
kapil <kapil707sharma@gmail.com>
2023-10-17 14:17:11 +0530

    add

--- theme/parasharguru/header.php
+++ theme/parasharguru/header.php
@@ -8,8 +8,8 @@
 <![endif]-->
 
 <head>
-    <!-- TITLE OF SITE -->
-    <?php echo vp_head(); ?>
+	<?php echo vp_head(); ?>
+
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
@@ -21,4 +21,6 @@
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+
+	<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 </head>
