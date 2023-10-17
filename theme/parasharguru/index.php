427a8c3d2e46d3a04396829571adf7804f8e641c
kapil <kapil707sharma@gmail.com>
2023-10-17 16:27:13 +0530

    add

--- theme/parasharguru/index.php
+++ theme/parasharguru/index.php
@@ -51,8 +51,7 @@ $result1 = get_table("tbl_o_interest");
 ?>
 <div class="row">
     <div class="col-sm-12">                
-        <h2>Your Profile</h2>
-        <hr>
+        <h2 class="home_page_profile_h2">Your Profile</h2>
     </div>
     <div class="col-sm-12 form-group">
         <label>First Name:</label>
