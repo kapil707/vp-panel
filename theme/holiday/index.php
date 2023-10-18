02f0c02acda95b02d81fdc58a978dbc058e8370e
kapil <kapil707sharma@gmail.com>
2023-10-18 17:06:14 +0530

    add

--- theme/holiday/index.php
+++ theme/holiday/index.php
@@ -3,7 +3,7 @@
         <title>test page</title>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="<?php echo get_theme_path(); ?>css/bootstrap.css">
 
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.slim.min.js"></script>
         <script src="<?php echo get_theme_path(); ?>js/bootstrap.min.js"></script>
