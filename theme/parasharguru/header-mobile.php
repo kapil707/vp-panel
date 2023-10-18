5f0795a3757f3ab4164980fdd72d5bd05fa5248f
kapil707 <kapil707sharma@gmail.com>
2023-10-18 14:15:19 +0530

    add

--- theme/parasharguru/header-mobile.php
+++ theme/parasharguru/header-mobile.php
@@ -95,7 +95,7 @@ $users_row = get_table_row("tbl_o_my_users where id='$id'");
 </div>
 <div class="fix-header bg-[#A17603]">
 	<div class="col-sm-12">
-		<div class="bg-[#A17603] text-white text-center p-1">
+		<div class="text-white text-center p-1">
 			<img src="<?php echo get_theme_path(); ?>images/menu-icon-12.png" class="mobile-menu-css" onclick="menushow()">
 			<p class="font-medium text-[28px] mt-1 main-header-text" id="join">
 				<?php echo get_field_data('login_label1','73'); ?>
