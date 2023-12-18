0342e5e9e3195540d6461a9669b92afdb7b1aae2
Kapil <kapil@Kapils-MacBook-Air.local>
2023-12-18 11:06:31 +0530

    add

--- theme/parasharguru/index.php
+++ theme/parasharguru/index.php
@@ -9,15 +9,15 @@
 
 <div class="container-fluid image-container1" style="background-image: url(<?php echo get_theme_path(); ?>images/box/bg.jpeg);">
     <div class="row">
-        <div class="col-sm-4 col-12">
+        <div class="col-sm-4 col-12 text-center">
             <img src="<?php echo get_theme_path(); ?>images/box/1.jpeg" class="card-img-top" alt="">
             <a href="" class="menu-button-css">Learning</a>
         </div>
-        <div class="col-sm-4 col-12">
+        <div class="col-sm-4 col-12 text-center">
             <img src="<?php echo get_theme_path(); ?>images/box/2.jpeg" class="card-img-top" alt="">
             <a href="" class="menu-button-css">Learning</a>
         </div>
-        <div class="col-sm-4 col-12">
+        <div class="col-sm-4 col-12 text-center">
             <img src="<?php echo get_theme_path(); ?>images/box/3.jpeg" class="card-img-top" alt="">
             <a href="" class="menu-button-css">Learning</a>
         </div>
