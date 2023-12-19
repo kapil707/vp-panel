2e69319957f285d8e8b904b664302d9cd3ab7ca1
Kapil <kapil@Kapils-MacBook-Air.local>
2023-12-19 13:27:01 +0530

    add

--- theme/parasharguru/login.php
+++ theme/parasharguru/login.php
@@ -2,7 +2,7 @@
 <div class="container">
 	<div class="row justify-content-center">
 		<div class="col-sm-4 text-center">
-			<img src="<?php echo $logo = get_library_to_image(get_field_data("image_site_logo"),'main'); ?>" width="40%">
+			<img src="<?php echo $logo = get_library_to_image(get_field_data("image_site_logo"),'main'); ?>" width="100%">
 			<div>
 				<p class="text-white">
 					<?php echo get_field_data('login_label3','73'); ?>
