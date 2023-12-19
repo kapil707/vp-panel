26e42baa079268efe3158a2112c13e66e2d8f7d9
Kapil <kapil@Kapils-MacBook-Air.local>
2023-12-19 13:27:28 +0530

    add

--- theme/parasharguru/login.php
+++ theme/parasharguru/login.php
@@ -1,7 +1,7 @@
 <?php include_once(get_header()); ?>
 <div class="container">
 	<div class="row justify-content-center">
-		<div class="col-sm-4 text-center">
+		<div class="col-sm-4">
 			<img src="<?php echo $logo = get_library_to_image(get_field_data("image_site_logo"),'main'); ?>" width="100%">
 			<div>
 				<p class="text-white">
