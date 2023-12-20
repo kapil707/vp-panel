f0b2932f69a6d08e3ab62419e930026966dea6e5
kapil-asus-new <kapil707sharma@gmail.com>
2023-12-20 15:40:40 +0530

    add

--- theme/parasharguru/referral-code.php
+++ theme/parasharguru/referral-code.php
@@ -4,12 +4,12 @@ $id = $_SESSION['profile_user'];
 			
 $row = get_table_row("tbl_o_my_users where id='$id'");
 ?>
-<h1 style="font-size:30px;margin-top:30px;" class="text-center">Referral code</h1>
-<h1 style="font-size:30px;margin-top:30px;" class="text-center"><?php echo $row->your_code ?></h1>
 <div class="container-fluid">
     <div class="row">
         <div class="col-sm-2"></div>
 		<div class="col-sm-8 text-white p-5">
+            <h2 style="font-size:30px;margin-top:30px;" class="text-center">Referral code</h1>
+            <h2 style="font-size:30px;margin-top:30px;" class="text-center"><?php echo $row->your_code ?></h1>
             <?php include_once('referral-code2.php'); ?>
         </div>
     </div>
