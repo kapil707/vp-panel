67d3d0900d8b9bec0edf4b175561ca516c8fab2c
kapil-asus-new <kapil707sharma@gmail.com>
2023-12-20 15:34:03 +0530

    add

--- theme/parasharguru/edit-profile.php
+++ theme/parasharguru/edit-profile.php
@@ -26,24 +26,24 @@ $result1 = get_table("tbl_o_interest");
 <div class="container-fluid">
     <div class="row">
         <div class="col-sm-2"></div>
-		<div class="col-sm-8">
+		<div class="col-sm-8 text-white">
 			<form method="POST" class="detailsbox" id="contact-form" enctype="multipart/form-data" action="<?php echo base_url(); ?>post-data">
 				<input type="hidden" name="action_type" value="profile_page_form_submit">
 				<div class="row">
-					<div class="col-sm-12 form-group">
+					<div class="col-sm-6 form-group">
 						<label>First Name:</label>
 						<input type="text" name="first_name" class="form-control input-lg" placeholder="First Name" required="" value="<?php echo $row->first_name ?>">
 					</div>
-					<div class="col-sm-12 form-group">
+					<div class="col-sm-6 form-group">
 						<label>Last Name:</label>
 						<input type="text" name="last_name" class="form-control input-lg" placeholder="Last Name" required="" value="<?php echo $row->last_name ?>">
 					</div>
-					<div class="col-sm-12 form-group">
+					<div class="col-sm-6 form-group">
 						<label>Email:</label>
 						<input type="text" name="email" class="form-control input-lg" placeholder="Enter Email" value="<?php echo $row->email ?>">
 					</div>
 					
-					<div class="col-sm-12 form-group">
+					<div class="col-sm-6 form-group">
 						<label>Phone Number</label>	
 						<input type="text" name="" class="form-control input-lg" placeholder="Enter Phone Number" required="" value="<?php echo $row->mobile ?>" readonly>
 					</div>
@@ -54,7 +54,7 @@ $result1 = get_table("tbl_o_interest");
 						$interest = $row->interest;
 					}
 					?>
-					<div class="col-sm-12 form-group">
+					<div class="col-sm-6 form-group">
 						<label>Interested in</label>	
 						<select name="interest" id="interest" class="form-control input-lg" onchange="onchnage_dropdown()">
 						<option value="0" <?php if($interest=="0") {echo "selected"; } ?>>Please Select</option>
@@ -72,7 +72,7 @@ $result1 = get_table("tbl_o_interest");
 					
 					$result3 = get_table("tbl_o_interest_type where interest_id='$row2->id'");
 					?>
-					<div class="col-sm-12 form-group">
+					<div class="col-sm-6 form-group">
 						<label><?php echo $row2->name ?></label>	
 						<select name="interest_type" id="interest_type" class="form-control input-lg">
 							<option value="0">Please Select</option>
@@ -85,22 +85,22 @@ $result1 = get_table("tbl_o_interest");
 					</div>
 					<?php } ?>
 					
-					<div class="col-sm-12 form-group">
+					<div class="col-sm-6 form-group">
 						<label>Address:</label>
 						<textarea cols="4" rows="3" class="wpcf7-form-control wpcf7-textarea form-control" aria-invalid="false" placeholder="Your message" name="address"><?php echo $row->address ?></textarea>
 					</div>
 					
-					<div class="col-sm-12 form-group">
+					<div class="col-sm-6 form-group">
 						<label>Date Of Birth:</label>
 						<input type="date" name="dob" class="form-control input-lg" placeholder="Date Of Birth" required="" value="<?php echo $row->dob ?>">
 					</div>
 					
-					<div class="col-sm-12 form-group">
+					<div class="col-sm-6 form-group">
 						<label>Time:</label>
 						<input type="time" name="time" class="form-control input-lg" placeholder="Time" required="" value="<?php echo $row->time ?>">
 					</div>
 					
-					<div class="col-sm-12 form-group">
+					<div class="col-sm-6 form-group">
 						<label>Place:</label>
 						<input type="text" name="place" class="form-control input-lg" placeholder="Place" required="" value="<?php echo $row->place ?>">
 					</div>
