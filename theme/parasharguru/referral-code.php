f51daa463ffdabfcfd89f4bd630ade208a750e92
kapil-asus-new <kapil707sharma@gmail.com>
2023-12-20 15:39:57 +0530

    add

--- theme/parasharguru/referral-code.php
+++ theme/parasharguru/referral-code.php
@@ -6,6 +6,12 @@ $row = get_table_row("tbl_o_my_users where id='$id'");
 ?>
 <h1 style="font-size:30px;margin-top:30px;" class="text-center">Referral code</h1>
 <h1 style="font-size:30px;margin-top:30px;" class="text-center"><?php echo $row->your_code ?></h1>
-	
-<?php include_once('referral-code2.php'); ?>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-2"></div>
+		<div class="col-sm-8 text-white p-5">
+            <?php include_once('referral-code2.php'); ?>
+        </div>
+    </div>
+</div>
 <?php include_once(get_footer("mobile")); ?>
