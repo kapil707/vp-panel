757b09c62303e179aecfa342a0b5934ffd7a4df6
kapil-asus-new <kapil707sharma@gmail.com>
2023-12-22 15:58:01 +0530

    add

--- theme/parasharguru/panchang.php
+++ theme/parasharguru/panchang.php
@@ -3,6 +3,7 @@
     <div class="row">
         <div class="col-sm-2"></div>
 		<div class="col-sm-8 text-white p-5">
+			
 			<?php include_once('panchang-2.php'); ?>
 			<hr style="margin-top:50px;">
 			<p style="margin-top:50px;">
