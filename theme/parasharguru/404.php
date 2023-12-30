eafd6369c495b604d26527f2251fde1411546e9f
kapil-asus-new <kapil707sharma@gmail.com>
2023-12-30 11:58:24 +0530

    add

--- theme/parasharguru/404.php
+++
@@ -1,3 +0,0 @@
-<?php include_once(get_header("mobile")); ?>
-<h1>Page Not Found</h1>
-<?php include_once(get_footer("mobile")); ?>
--- theme/parasharguru/profile-logout.php
+++
@@ -1,4 +0,0 @@
-<?php 
-session_destroy();
-redirect(base_url().'home');
-?>
